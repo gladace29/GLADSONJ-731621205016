af8f0694f36405beb1b56dbd03b08adebf347094
gladace29 <jpglad29@gmail.com>
2024-07-31 09:08:40 +0530

    Angular Day 2

---
+++ Angular Workspace/src Day2/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name : string;
+  age : number;
+  email : string;
+  imagePath : string;
+  fname : String;
+  flag : boolean;
+  vehicles : string[];
+  selectedVehicle : string;
+  myStyle : {};
+  myClass : string;
+
+  constructor(){
+    this.name = "Gladson";
+    this.age = 21;
+    this.email ="jpglad29@gmail.com";
+    this.imagePath = "https://picsum.photos/200/300";
+    this.fname = "";
+    this.flag = true;
+    this.vehicles = ["TwoWheeler","ThreeWheeler","FourWheeler"];
+    this.selectedVehicle = "";
+    this.myStyle = {'width' : '40%','border':'2px solid green'};
+    this.myClass = "MyClass1";
+  }
+
+  ChangeName(){
+    this.name ="Ace";
+  }
+
+  ChangeAge(){
+    this.age=22;
+  }
+
+  ChangeEmail(){
+
+    this.email = "gladsonace29@gamil.com";
+  }
+
+  ChangeImage(){
+    this.imagePath="https://lh3.googleusercontent.com/Ryg5ih-fOWbpEGDpbJYJz5RTq3_28Tvo2h3JQRCdiz16lw5ghitDMN6hcWA57g0d6_8VOqQYcCemRBOfZGQA64ZKkFOn71zuF1LDsXwCh4ywF-_z6VqxbBqmbQvK_-PWfZcztxDd1WDTEEUHX6GdP60";
+  }
+
+  ChangeFlag(){
+    this.flag = ! this.flag;
+  }
+
+  setSelectedItem(vec : string){
+   this.selectedVehicle = vec;
+  }
+
+  ChangeStyle(){
+    this.myStyle = {'width' : '40%','border':'2px solid aqua'};
+  }
+}
+
+
---
+++ Angular Workspace/src Day2/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GladComponent } from './glad/glad.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GladComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
